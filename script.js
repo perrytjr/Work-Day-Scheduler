8f52199a3d9e57b736d2c393109c1bd82742ec33
Perry Theobald <perrytjr@gmail.com>
2020-08-03 22:58:46 -0700

    i think colors are working for hours

--- script.js
+++ script.js
@@ -4,33 +4,62 @@ $(document).ready(function () {
     $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 
 });
+
 //work day scheduler container
 const dayPart = $('.container');
 
 
 // hour variables
-const hours = ['9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm'];
+const dayHours = ['9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm'];
 const twentyfourhourClock = ['9', '10', '11', '12', '13', '14', '15', '16', '17'];
 
 //builds the work day scheduler container
 
-for (var i = 0; i < hours.length; i++) {
-const rowDiv = $("<div class= 'row time-block'>").attr("id", twentyfourhourClock[i]);
-const hourDiv = $("<div class='hour col-md-1'>");
-const txtDiv = $("<textarea class='col-md-10 description'>");
-const btnDiv = $("<button type='button' class='btn saveBtn col-md-1 fas fa-save'>");
+for (var i = 0; i < dayHours.length; i++) {
+    const rowDiv = $("<div class= 'row time-block'>").attr("id", twentyfourhourClock[i]);
+    const hourDiv = $("<div class='hour col-md-1'>");
+    const txtDiv = $("<textarea class='col-md-10 description'>");
+    const btnDiv = $("<button type='button' class='btn saveBtn col-md-1 fas fa-save'>");
 
-dayPart.append(rowDiv);
+    dayPart.append(rowDiv);
 
-hourDiv.text(hours[i]);
-rowDiv.append(hourDiv);
+    hourDiv.text(dayHours[i]);
+    rowDiv.append(hourDiv);
 
-txtDiv.text();
-rowDiv.append(txtDiv);
+    txtDiv.text();
+    rowDiv.append(txtDiv);
 
-btnDiv.text();
-rowDiv.append(btnDiv);
+    btnDiv.text();
+    rowDiv.append(btnDiv);
 }
+
+
+
+
+
+
+function businessHours() {
+    var rightNow = moment().hours()
+    $('.time-block').each(function () {
+        const hourPool = parseInt($(this).attr("id").split(" ")[0]);
+
+        if (hourPool < rightNow) {
+            $(this).addClass("past");
+
+        } else if (hourPool === rightNow) {
+            $(this).removeclass("past");
+            $(this).addclass("present");
+        } else {
+            $(this).removeclass("past");
+            $(this).removeclass("present");
+            $(this).addclass("future");
+        }
+
+    });
+
+}
+businessHours();
+//var checkTime = setInterval(businessHours, 15000);
 
 
 
